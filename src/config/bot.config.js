ea52e6cde574a9f2e74d6a8f95f12f6083d6d965
mvazquez <matias.vazquezmartin@gmail.com>
2024-01-25 18:13:33 -0300

    feat: add slash commands

--- src/config/bot.config.js
+++ src/config/bot.config.js
@@ -2,4 +2,6 @@ require('dotenv').config();
 
 module.exports = {
   TOKEN: process.env.TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
+  GUILD_ID: process.env.GUILD_ID,
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('node:path');
 const { TOKEN } = require('./config/bot.config');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { CONNREFUSED } = require('dns');
 
 const client = new Client({
   intents: [
@@ -34,14 +35,36 @@ for (const folder of commandFolders) {
   }
 }
 
-
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, (interaction) => {
+client.on(Events.InteractionCreate, async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  console.log(interaction);
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error while executing this command!',
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: 'There was an error while executing this command!',
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 client.login(TOKEN);
