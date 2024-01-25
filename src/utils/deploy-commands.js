39c6635066078d19de1730636fff5113783cf9ee
mvazquez <matias.vazquezmartin@gmail.com>
2024-01-25 18:10:58 -0300

    feat: deploy-commands

---
+++ src/utils/deploy-commands.js
@@ -0,0 +1,54 @@
+require('dotenv').config({ path: '../../.env' });
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('node:path');
+
+const foldersPath = path.join(__dirname, '../commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+const config = {
+  TOKEN: process.env.TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
+  GUILD_ID: process.env.GUILD_ID,
+};
+const { TOKEN, CLIENT_ID, GUILD_ID } = config;
+
+const commands = [];
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+const rest = new REST().setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+    const data = await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands }
+    );
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    console.error(error);
+  }
+})();
